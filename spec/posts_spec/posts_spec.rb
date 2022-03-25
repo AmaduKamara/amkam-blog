814f379179b977b9a7c1c841f0f71d51b4a94adc
Amkam <amaduskamara@gmail.com>
2022-03-25 07:50:09 +0000

    Comment tzinfo-data duplicate Gemfile and fix linter errors

--- spec/posts_spec/posts_spec.rb
+++ spec/posts_spec/posts_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-
   describe 'GET post' do
-    before(:each) {get '/users/:user_id/posts'}
+    before(:each) { get '/users/:user_id/posts' }
 
     it 'should render a template' do
       expect(response).to render_template('posts/index')
     end
 
-    it "has a 200 status code" do
-       expect(response).to have_http_status(:ok)
+    it 'has a 200 status code' do
+      expect(response).to have_http_status(:ok)
     end
 
     it 'should test heading text inside template' do
--- spec/users_spec/users_spec.rb
+++ spec/users_spec/users_spec.rb
@@ -1,16 +1,15 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Users", :type => :request do
-
-  describe "GET index" do
+RSpec.describe 'Users', type: :request do
+  describe 'GET index' do
     before(:each) { get '/users' }
 
-    it "it should render a template" do
+    it 'it should render a template' do
       expect(response).to render_template('users/index')
     end
 
-    it "has a 200 status code" do
-       expect(response).to have_http_status(:ok)
+    it 'has a 200 status code' do
+      expect(response).to have_http_status(:ok)
     end
 
     it 'should test heading text inside template' do
