3eaf9c4871b3ee3e194399b33bb6b25fe6a71082
Amkam <amaduskamara@gmail.com>
2022-03-30 13:19:14 +0000

    Add after_save on post, likes and comment increment_counter methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,15 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :author, class_name: :User
-
-  def update_post_counter
-    author.increment!(:posts_counter)
-  end
+  after_save :update_post_counter
 
   def recent_comments
     comments.limit(5).order(created_at: :desc)
+  end
+
+  private
+
+  def update_post_counter
+    author.increment!(:posts_counter)
   end
 end
