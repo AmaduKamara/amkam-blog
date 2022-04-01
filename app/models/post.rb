8766b63c709c23446823de936183d80ddbb1ae70
Amkam <amaduskamara@gmail.com>
2022-04-01 20:08:27 +0000

    Add validations for Post and User models

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: :User
   after_save :update_post_counter
 
+  validates :title, length: 1..250, presence: true, allow_blank: false
+  validates :comments_counter, numericality: { only_integer: true, greater_than: -1 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than: -1 }
+
   def recent_comments
     comments.limit(5).order(created_at: :desc)
   end
