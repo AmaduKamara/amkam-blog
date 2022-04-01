e691dbad6c169447f34b58502566d325f9f40d1f
Amkam <amaduskamara@gmail.com>
2022-04-01 15:27:33 +0000

    Fix linter errors with rubocop

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,7 @@
 class LikesController < ApplicationController
   def create
     print current_user
-    new_like = current_user.likes.new(like_params) # (author_id: params[:like][:author_id], post_id: params[:like][:post_id])
+    new_like = current_user.likes.new(like_params)
 
     if new_like.save
       redirect_to params[:like][:current_url], notice: 'Success!'
